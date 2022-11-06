de1b357a68304a2ec362e4356a1a79a0bbf16fbf
Lathifnurirsyad <lathifni.ni@gmail.com>
2022-11-06 17:49:51 +0700

    typo

--- database/migrations/2022_11_04_041504_create_produk_table.php
+++ database/migrations/2022_11_04_041504_create_produk_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("nama",50);
             $table->int("status");
-            $table->int("harga");
+            $table->int("hargaaaaaaaaaaaaaa");
             $table->timestamps();
         });
     }
