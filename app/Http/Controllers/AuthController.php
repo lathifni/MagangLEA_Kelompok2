b56a45bc4556aeb8c84536a39c628b0ba1a76fb8
Gilang2812 <gilang281201@gmail.com>
2022-11-26 00:30:35 +0700

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
@@ -83,6 +84,7 @@ class AuthController extends Controller
             'no_hp'=>$request->no_hp,
             'jenis_kelamin'=>$request->jenis_kelamin
         ];
+        DB::table('users')->insert($data);
         User::create($data);
         return redirect('login');
     }
